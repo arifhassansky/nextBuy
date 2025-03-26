84c000635d8c47b8d2bb3bebc776395a81cdac4a
Muhammad Ranju <mdranju23@gmail.com>
2025-03-27 02:10:14 +0600

    feat: Update Home component layout and enhance ProductsCard functionality

--- src/components/productsCard/ProductsCard.tsx
+++ src/components/productsCard/ProductsCard.tsx
@@ -1,27 +1,30 @@
-'use client';
-import LoadingSpinner from '@/components/loadingSpinner/LoadingSpinner';
-import { Card } from '@/components/ui/Card/Card';
-import { useGetProductsQuery } from '@/redux/ProductApi';
+"use client";
+import LoadingSpinner from "@/components/loadingSpinner/LoadingSpinner";
+import { Card } from "@/components/ui/Card/Card";
+import { useGetProductsQuery } from "@/redux/ProductApi";
 
 const ProductsCard = () => {
-   const { data, error, isLoading } = useGetProductsQuery();
-   const products = data?.data || [];
+  const { data, error, isLoading } = useGetProductsQuery();
+  const products = data?.data || [];
 
-   if (isLoading) return <LoadingSpinner />;
-   if (error) return <div>Error: {error?.message || 'Failed to load products.'}</div>;
+  if (isLoading) return <LoadingSpinner />;
+  if (error)
+    return <div>Error: {error?.message || "Failed to load products."}</div>;
 
-   return (
-      <div className='w-11/12 mx-auto px-4'>
-         {/* Grid layout for products */}
-         <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8'>
-            {products.length > 0 ? (
-               products.map(product => <Card key={product._id} product={product} />)
-            ) : (
-               <div>No products available.</div>
-            )}
-         </div>
+  return (
+    <div className="w-11/12 mx-auto px-4">
+      {/* Grid layout for products */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8">
+        {products.length > 0 ? (
+          products.map((product) => (
+            <Card key={product._id} product={product} />
+          ))
+        ) : (
+          <div>No products available.</div>
+        )}
       </div>
-   );
+    </div>
+  );
 };
 
 export default ProductsCard;
