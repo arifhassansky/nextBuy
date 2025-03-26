9d770221925af06f781cdf45970276dece76a639
himel1625 <himelmia1625@gmail.com>
2025-03-27 02:12:07 +0600

    Fixed the error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,10 @@
-<<<<<<< HEAD
-import Category from "@/components/Category/Category";
-import Navbar from "@/components/Navbar/Navbar";
-import Quality from "@/components/Quality/Quality";
-import Slider from "@/components/Slider/Slider";
-
-export default function Home() {
-  return (
-    <>
-      <Navbar />
-      <Slider />
-      <Category />
-      <Quality />
-    </>
-  );
-=======
 import Footer from '@/components/Footer/Footer';
 import LatestArticlesSlider from '@/components/LatestArticlesSlider/LatestArticlesSlider';
 import Navbar from '@/components/Navbar';
-import ProductsCard from '@/components/productsCard/productsCard';
-import Slider from '@/components/Slider';
+import ProductsCard from '@/components/productsCard/ProductsCard';
+// import ProductsCard from '@/components/productsCard/productsCard';
+import Slider from '@/components/Slider/Slider';
+// import Slider from '@/components/Slider';
 import TechnicalSpecifications from '@/components/TechnicalSpecifications/TechnicalSpecifications';
 
 export default function Home() {
@@ -26,16 +12,10 @@ export default function Home() {
       <>
          <Navbar />
          <Slider />
-<<<<<<< HEAD
          <TechnicalSpecifications />
          <ProductsCard />
-=======
-         <TechnicalSpecifications/>
-         <LatestArticlesSlider/>
-         <Card />
->>>>>>> 94106403f3258c2737afa4ef042995b67feec5fe
+         <LatestArticlesSlider />
          <Footer />
       </>
    );
->>>>>>> a070efd0c83466948146cdc90042c17c850f7c3b
 }
