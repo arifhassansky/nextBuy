00c4db055ba07d2b95b933977fdb299dcef3aeac
unknown <arifhassansky@gmail.com>
2025-03-28 03:22:36 +0600

    added wishlist and add to card for user

--- next.config.mjs
+++ next.config.mjs
@@ -19,10 +19,14 @@ const nextConfig = {
         hostname: "lh3.googleusercontent.com",
       },
       {
-        protocol: 'https',
-        hostname: 'woodmart.xtemos.com',
-        port: '',
-        pathname: '/wp-content/uploads/**',
+        protocol: "https",
+        hostname: "woodmart.xtemos.com",
+        port: "",
+        pathname: "/wp-content/uploads/**",
+      },
+      {
+        protocol: "https",
+        hostname: "img.daisyui.com",
       },
     ],
   },
