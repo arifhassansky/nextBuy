a3b193c1e858a9fdb5941b48c99c9032fbf9a9a5
unknown <abir045@gmail.com>
2025-03-27 14:41:06 +0600

    updated navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Category from "@/components/Category/Category";
 import Footer from "@/components/Footer/Footer";
 import LatestArticlesSlider from "@/components/LatestArticlesSlider/LatestArticlesSlider";
+import ResponsiveNavbar from "@/components/megamenu/ResponsiveNavbar";
 import Navbar from "@/components/Navbar/Navbar";
 import ProductsCard from "@/components/productsCard/productsCard";
 import Quality from "@/components/Quality/Quality";
@@ -11,6 +12,7 @@ export default function Home() {
   return (
     <>
       <Navbar />
+
       <Slider />
       <Category />
       <Quality />
