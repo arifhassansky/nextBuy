df4d283b2aeba424998e73a9a3d76b3ef82b35c3
Muhammad Ranju <mdranju23@gmail.com>
2025-03-27 16:24:24 +0600

    Refactor: Clean up Home component and add Branding section

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+import Branding from "@/components/Branding/Branding";
 import Category from "@/components/Category/Category";
 import Footer from "@/components/Footer/Footer";
 import LatestArticlesSlider from "@/components/LatestArticlesSlider/LatestArticlesSlider";
@@ -9,7 +9,6 @@ import Slider from "@/components/Slider/Slider";
 import TechnicalSpecifications from "@/components/TechnicalSpecifications/TechnicalSpecifications";
 
 export default function Home() {
-  console.log(process.env.NEXTAUTH_URL);
   return (
     <>
       <Slider />
@@ -18,36 +17,10 @@ export default function Home() {
       <TechnicalSpecifications />
       <ProductsCard />
       <LatestArticlesSlider />
+      <Branding />
       <NewsletterSection />
       {/* <ProductsCard /> */}
       <Footer />
     </>
   );
-=======
-import Branding from '@/components/Branding/Branding';
-import Category from '@/components/Category/Category';
-import Footer from '@/components/Footer/Footer';
-import LatestArticlesSlider from '@/components/LatestArticlesSlider/LatestArticlesSlider';
-import NewsletterSection from '@/components/NewsletterSection/NewsletterSection';
-import ProductsCard from '@/components/productsCard/ProductsCard';
-import Quality from '@/components/Quality/Quality';
-import Slider from '@/components/Slider/Slider';
-import TechnicalSpecifications from '@/components/TechnicalSpecifications/TechnicalSpecifications';
-
-export default function Home() {
-   return (
-      <>
-         <Slider />
-         <Category />
-         <Quality />
-         <TechnicalSpecifications />
-         <ProductsCard />
-         <LatestArticlesSlider />
-         <Branding/>
-         <NewsletterSection />
-         {/* <ProductsCard /> */}
-         <Footer />
-      </>
-   );
->>>>>>> bc11d1912b051e6731ff4c9cad2770bb1a292e68
 }
