674c9e004e8a6a6a3ffdf27b4f05d1e0f44ff0c4
unknown <arifhassansky@gmail.com>
2025-03-28 22:31:49 +0600

    added solve

--- src/app/(home)/products/page.tsx
+++ src/app/(home)/products/page.tsx
@@ -1,16 +1,17 @@
 import ProductsPage from "./ProductsPage";
+
 interface Props {
-  searchParams: { [key: string]: string | string[] | undefined };
-  search: string[] | string;
+  searchParams: Record<string, string | string[] | undefined>;
 }
 
 const Products = ({ searchParams }: Props) => {
-  const search = searchParams.search || "";
+  const search = searchParams.search ?? "";
 
   console.log(search);
+
   return (
     <div className="w-11/12 mx-auto px-4">
-      <ProductsPage search={search as string[]} />
+      <ProductsPage search={Array.isArray(search) ? search : [search]} />
     </div>
   );
 };
