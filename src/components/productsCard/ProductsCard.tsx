a449b2d16f5d27c63fbe0ea32589d240dabac0ee
AyeshaFerdous <ayeshaferdous01@gmail.com>
2025-03-29 02:40:50 +0600

    Added a All Blogs Page

--- src/components/productsCard/ProductsCard.tsx
+++ src/components/productsCard/ProductsCard.tsx
@@ -41,7 +41,7 @@ const ProductsCard: FC = () => {
   // Handle error state
   if (error) {
     const errorMessage =
-      (error as FetchError)?.data?.message 
+      (error as FetchError)?.data?.message|| 
       (error as FetchError)?.message 
       "Failed to load products.";
     return (
@@ -57,24 +57,27 @@ const ProductsCard: FC = () => {
       ? (data as ApiResponse).data
          : (data as Product[] | undefined);
    return (
+  
     <div className="md:w-11/12 mx-auto px-4">
       {/* Grid layout for products */}
       <h2 className="text-2xl font-bold mb-6 text-center md:text-left">
         OUR PRODUCTS
       </h2>
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 sm:gap-6 md:gap-8">
-        {isLoading && (
-          <>
-            {Array.from({ length: 5 }, (, i) => i + 1).map((, i) => (
-              <CardSkeleton key={i} />
-            ))}
-          </>
-        )}
+      {isLoading && (
+               <>
+                  {Array.from({ length: 5 }, (_, i) => i + 1).map((_, i) => (
+                     <CardSkeleton key={i} />
+                  ))}
+               </>
+            )}
+       
         {products?.map((product) => (
-          <Link key={product._id} href={/products/${product.slug}}>
+          <Link key={product._id} href={`/products/${product.slug}`}>
             <Card key={product?._id} product={product} />
           </Link>
         ))}
+       
       </div>
       <div className="flex justify-center items-center h-40 text-red-500 font-semibold">
         <Link href={"/products"}>
