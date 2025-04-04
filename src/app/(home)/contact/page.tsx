3f9d99290c6ee07c14813bd3c8e8d7bfe5e54b7d
mubashirbappi <mubashirbappi@gmail.com>
2025-04-04 20:22:49 +0600

    added contact page header

--- src/app/(home)/contact/page.tsx
+++ src/app/(home)/contact/page.tsx
@@ -3,7 +3,15 @@ import React from "react";
 const contact = () => {
   return (
     <div className="h-screen mt-64 text-center font-bold text-7xl">
-      contact page up coming
+       <div className="bg-[#3c9e26] text-white py-16 text-center">
+        <h1 className="text-4xl font-bold">Contact Us</h1>
+        <p className="mt-2 text-lg">We are here to assist you with any inquiries.</p>
+      </div>
+
+
+
+
+
     </div>
   );
 };
